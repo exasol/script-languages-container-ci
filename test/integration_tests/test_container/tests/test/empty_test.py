75f255d116cda626519c8418e29decc447888f46
Torsten Kilias <torsten.kilias@exasol.com>
2023-03-24 17:06:42 +0100

    Reduce test resources to the minimal needed

--- test/integration_tests/test_container/tests/test/empty_test.py
+++ test/integration_tests/test_container/tests/test/empty_test.py
@@ -6,7 +6,6 @@ from exasol_python_test_framework import docker_db_environment
 
 class EmptyTest(udf.TestCase):
 
-    @udf.skipIfNot(docker_db_environment.is_available, reason="This test requires a docker-db environment")
     def test_dummy(self):
         pass
 
