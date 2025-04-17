1b4fcfd62c498aa8d79ba11ac8412ddc2b01a011
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2025-04-17 13:43:05 -0300

    Fixed formatting

--- noxfile.py
+++ noxfile.py
@@ -3,8 +3,10 @@ import nox
 # imports all nox task provided by the toolbox
 from exasol.toolbox.nox.tasks import *  # type: ignore
 
-from exasol_script_languages_container_ci.lib.config.data_model_generator import config_data_model_default_output_file, \
-    generate_config_data_model
+from exasol_script_languages_container_ci.lib.config.data_model_generator import (
+    config_data_model_default_output_file,
+    generate_config_data_model,
+)
 
 # default actions to be run if nothing is explicitly specified with the -s option
 nox.options.sessions = ["project:fix"]
