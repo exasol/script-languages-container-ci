8a26dae116d96ef065ebb081b909f9af31b4562a
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2025-05-02 15:19:34 -0300

    #82: Implement CI for Github workflows - build-and-export

--- exasol/slc_ci/cli/commands/__init__.py
+++ exasol/slc_ci/cli/commands/__init__.py
@@ -1,3 +1,4 @@
 from .check_if_build_needed import check_if_build_needed
 from .get_build_runner import get_build_runner
 from .get_flavors import get_flavors
+from .export_and_scan_vulnerabilities import export_and_scan_vulnerabilities
---
+++ test/integration/github/test_ci_export.py
@@ -0,0 +1,18 @@
+from tempfile import TemporaryDirectory
+
+import pytest
+
+from exasol.slc_ci.lib.ci_export import CIExport
+
+
+@pytest.mark.parametrize(
+    "goal", ["release", "base_test_build_run"]
+)
+def test(flavors_path, goal):
+    flavor_name = "successful"
+    flavor_path = str(flavors_path / flavor_name)
+    with TemporaryDirectory() as temp_dir:
+        res = CIExport().export(flavor_path=(flavor_path,), goal=goal, output_directory=temp_dir)
+        assert res.exists()
+        assert res.name.endswith("tar.gz")
+        assert res.name.startswith(flavor_name)
