fe061044509ad7180f5e744ee1f3282e11c59537
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2025-05-27 16:16:42 +0200

    Added unit tests

--- test/integration/github/test_ci_prepare.py
+++ test/integration/github/test_ci_prepare.py
@@ -2,13 +2,14 @@ import datetime
 import os
 from pathlib import Path
 
+from exasol_integration_test_docker_environment.cli.options.system_options import DEFAULT_OUTPUT_DIRECTORY
 from exasol_integration_test_docker_environment.lib.logging import luigi_log_config
 
 from exasol.slc_ci.lib.ci_build import CIBuild
 from exasol.slc_ci.lib.ci_prepare import CIPrepare
 
 
-def test(flavors_path, mock_settings_env_vars):
+def test(flavors_path, mock_settings_env_vars, tmp_test_dir):
     test_type = "successful"
     flavor_path = str(flavors_path / test_type)
     CIPrepare().prepare("sha_123")
@@ -22,7 +23,7 @@ def test(flavors_path, mock_settings_env_vars):
     log_path = Path(os.environ[luigi_log_config.LOG_ENV_VARIABLE_NAME])
     assert log_path.is_file()
 
-    OUTPUT_PATH = Path(".build_output")
+    OUTPUT_PATH = Path(DEFAULT_OUTPUT_DIRECTORY)
     META_DATA_PATH = OUTPUT_PATH / "meta_data"
     date_path = META_DATA_PATH / "start_date"
     date = datetime.datetime.fromisoformat(date_path.read_text())
