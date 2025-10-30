0a3f7c9350508c87208eb453c74bb8db208427bd
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2025-10-30 12:39:56 -0300

    #128: Updated poetry dependencies and Github workflows by PTB 1.12.0 and removed Python 3.9 (#129)
    fixes  #128

--- doc/changes/unreleased.md
+++ doc/changes/unreleased.md
@@ -1,1 +1,5 @@
 # Unreleased
+
+## Refactorings
+
+ - #128: Updated poetry dependencies and Github workflows by PTB 1.12.0 and removed Python 3.9
--- noxconfig.py
+++ noxconfig.py
@@ -1,14 +1,12 @@
 from __future__ import annotations
 
 from collections.abc import Iterable
-from dataclasses import dataclass
 from pathlib import Path
 
-from nox import Session
+from exasol.toolbox.config import BaseConfig
 
 
-@dataclass(frozen=True)
-class Config:
+class Config(BaseConfig):
     root: Path = Path(__file__).parent
     source: Path = Path(__file__).parent / "exasol"
     doc: Path = Path(__file__).parent / "doc"
@@ -20,7 +18,10 @@ class Config:
         "resources",
         "config_data_model.py",
     )
-    python_versions = ["3.9", "3.10", "3.11", "3.12", "3.13"]
 
 
-PROJECT_CONFIG = Config()
+PROJECT_CONFIG = Config(
+    python_versions=("3.10", "3.11", "3.12", "3.13"),
+    exasol_versions=(),
+    create_major_version_tags=False,
+)
