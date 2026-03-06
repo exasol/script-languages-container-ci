fe65f7eeb3254f7ba20e2e7307159662d2257f14
Shankar.KV <shankar.vaithiyanadhaswamy@exasol.com>
2026-03-06 19:02:52 +0530

    fixing the test-runners

--- test/unit/test_get_test_matrix.py
+++ test/unit/test_get_test_matrix.py
@@ -8,32 +8,62 @@ EXPECTED_VALUES = {
     "include": [
         {
             "test-set-name": "all",
-            "test-runners": ["ubuntu-22.04", "ubuntu-22.04-arm"],
+            "test-runners": "ubuntu-22.04",
+            "goal": "release",
+        },
+        {
+            "test-set-name": "all",
+            "test-runners": "ubuntu-22.04-arm",
             "goal": "release",
         },
         {
             "test-set-name": "pandas_dtype_leak",
-            "test-runners": ["ubuntu-22.04", "ubuntu-22.04-arm"],
+            "test-runners": "ubuntu-22.04",
+            "goal": "release",
+        },
+        {
+            "test-set-name": "pandas_dtype_leak",
+            "test-runners": "ubuntu-22.04-arm",
             "goal": "release",
         },
         {
             "test-set-name": "pandas_dataframe_leak",
-            "test-runners": ["ubuntu-22.04", "ubuntu-22.04-arm"],
+            "test-runners": "ubuntu-22.04",
+            "goal": "release",
+        },
+        {
+            "test-set-name": "pandas_dataframe_leak",
+            "test-runners": "ubuntu-22.04-arm",
             "goal": "release",
         },
         {
             "test-set-name": "pandas",
-            "test-runners": ["ubuntu-22.04", "ubuntu-22.04-arm"],
+            "test-runners": "ubuntu-22.04",
+            "goal": "release",
+        },
+        {
+            "test-set-name": "pandas",
+            "test-runners": "ubuntu-22.04-arm",
             "goal": "release",
         },
         {
             "test-set-name": "generic",
-            "test-runners": ["ubuntu-22.04", "ubuntu-22.04-arm"],
+            "test-runners": "ubuntu-22.04",
+            "goal": "release",
+        },
+        {
+            "test-set-name": "generic",
+            "test-runners": "ubuntu-22.04-arm",
             "goal": "release",
         },
         {
             "test-set-name": "gpu",
-            "test-runners": ["gpu_test_runner_1", "gpu_test_runner_2"],
+            "test-runners": "gpu_test_runner_1",
+            "goal": "release",
+        },
+        {
+            "test-set-name": "gpu",
+            "test-runners": "gpu_test_runner_2",
             "goal": "release",
         },
     ]
